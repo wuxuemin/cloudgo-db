5af779d3e6990e6d57c5db7b7096673a4c9f45f7
wuxuemin <1739636817@qq.com>
2017-11-29 18:14:22 +0800

    Add files via upload

---
+++ cloudgo-db-orm/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"cloudgo-db/cloudgo-db-orm/service"
+
+	flag "github.com/spf13/pflag"
+)
+
+// PORT port for server to listen
+const (
+	PORT string = "8080"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = PORT
+	}
+
+	pPort := flag.StringP("port", "p", PORT, "PORT for httpd listening")
+	flag.Parse()
+	if len(*pPort) != 0 {
+		port = *pPort
+	}
+
+	server := service.NewServer()
+	server.Run(":" + port)
+}
